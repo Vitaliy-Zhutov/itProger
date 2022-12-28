fbb36a8f91a4a7c1f6ef55f5b7e746e22103113b
vitaly <vitaly.zutov@yandex.ru>
2022-12-28 23:41:05 +0700

    Урок 2. Условные операторы (if-else)

---
+++ Lesson/Lesson_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace project {
+    class Program {
+        static void Main() {
+            Console.Write("Enter name: ");
+            string role = Console.ReadLine()!;
+
+            if(role == "Admin") {
+                Console.Write("Enter user name: ");
+                string user_name = Console.ReadLine()!;
+                Console.Write("Enter {0} age: ", user_name);
+                short age = Convert.ToInt16(Console.ReadLine()!);
+
+                if(age <= 0 || age > 99) {
+                    Console.Write("Enter {0} age: ", user_name);
+                    age = Convert.ToInt16(Console.ReadLine()!);
+                }
+
+                if(age <= 0 || age > 99) {
+                    Console.WriteLine("Error");
+                    age = 0;
+                }
+                else
+                    Console.WriteLine("User age is " + age);
+
+            } else
+                Console.WriteLine("You are not admin!");
+
+
+        }
+    }
+}
