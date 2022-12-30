62b91a016137a78013d6e2c4ad479c6a4b0ace36
vitaly <vitaly.zutov@yandex.ru>
2022-12-30 23:21:33 +0700

    Урок 3. Цикл for, while

---
+++ Lesson/Lesson_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace project {
+    class Program {
+        static void Main() {
+           // for(float i = 200; i > 10; i /= 2)
+           //     Console.WriteLine("Element: {0}", i);
+
+           /* 
+           byte i = 0;
+           while(i < 10) {
+                Console.WriteLine("Element: {0}", i);
+                i++;    
+           }
+           */
+           
+           /*
+           byte i = 10;
+           while(i >= 1) {
+            Console.WriteLine("Element: {0}", i);
+                i--;
+           } 
+           */ 
+
+           /* 
+           bool isHasCar = true;
+           while(isHasCar) {
+                string end = Console.ReadLine()!;
+                if(end == "end")
+                    isHasCar = false;
+           }
+           */
+
+           for(short i = 0; i < 10; i++) {
+                //if(i > 5)
+                 //   break;
+
+                 if(i % 2 == 0)
+                    continue;
+
+                Console.WriteLine("El: " + i);
+           }
+
+
+        }
+    }
+}
